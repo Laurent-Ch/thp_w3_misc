60a21419d31287767b41825ac3b68482b3eb8d96
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-12 13:08:44 +0200

    first exercice done, rpsec respective file half done

---
+++ w3_d12/lib/multiples.rb
@@ -0,0 +1,36 @@
+def sum_of_3_or_5_multiples(number) 
+  if input_a_nat_num?(number) == false
+    return "Please enter a natural number"
+  end
+  nat_num_into_array = (1...number).to_a
+  total_sum = 0
+  nat_num_into_array.each do |i|
+    if multiple_of_3_or_5?(i) == true
+      total_sum += i
+    end
+  end
+  return total_sum
+end
+
+def multiple_of_3_or_5?(number)
+  number % 3 == 0 || number % 5 == 0 ? (return true) : (return false)
+end
+
+def input_a_nat_num?(input)
+  if !input.is_a? Integer
+    return false
+  end
+  input >= 0 ? (return true) : (return false)
+end
+
+
+puts sum_of_3_or_5_multiples(1000)
+puts sum_of_3_or_5_multiples(10)
+puts sum_of_3_or_5_multiples(11)
+puts sum_of_3_or_5_multiples(0)
+puts sum_of_3_or_5_multiples(3)
+puts sum_of_3_or_5_multiples(-1)
+puts sum_of_3_or_5_multiples(4.7)
+puts sum_of_3_or_5_multiples("cat")
+
+
